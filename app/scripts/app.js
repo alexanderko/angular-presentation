671d2cd506066af02ad7507cfb5a1a8b35362edc
Alexander Kotov <alexanderko@interlink-ua.com>
2014-05-14 22:43:53 +0300

    remove initial view sample

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,10 +6,6 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
       .when('/slide', {
         templateUrl: 'views/slide.html',
         controller: 'SlideCtrl',
